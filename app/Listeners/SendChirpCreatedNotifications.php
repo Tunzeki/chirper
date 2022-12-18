c48a01ed3f7021fd65231ac5ab5f8839b9815129
tunzeki <tundewebhead@gmail.com>
2022-12-18 23:17:38 +0100

    Update the listener to send notifications
    ...to every user in the platform, except the author of the Chirp.

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -3,10 +3,12 @@
 namespace App\Listeners;
 
 use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendChirpCreatedNotifications
+class SendChirpCreatedNotifications implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -26,6 +28,8 @@ class SendChirpCreatedNotifications
      */
     public function handle(ChirpCreated $event)
     {
-        //
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
     }
 }
